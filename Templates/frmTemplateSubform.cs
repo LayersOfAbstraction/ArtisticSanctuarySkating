72c6e2bb5ee2c3f3679959ff67294457bb70dc77
Jordan Nash <LayersOfAbstraction@users.noreply.github.com>
2019-01-23 04:33:39 +1000

    Presentation Layer (templates)

---
+++ Templates/frmTemplateSubform.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ArtisticSanctuarySkating
+{
+    public partial class frmTemplateSubform : Form
+    {
+        public frmTemplateSubform()
+        {
+            InitializeComponent();
+            lblAlert.ResetText();
+            btnSave.Enabled = false;            
+        }
+
+        private void btnNew_Click(object sender, EventArgs e)
+        {
+            foreach (Control ctrl in Controls)
+            {
+                if (ctrl is TextBox || ctrl is ComboBox || ctrl is DateTimePicker)
+                {
+                    ctrl.ResetText();
+                }
+            }
+            btnCancel.Enabled = true;
+            btnSave.Enabled = true;
+            btnNew.Enabled = false;
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ Templates/frmTemplateSubform.designer.cs
@@ -0,0 +1,292 @@
+﻿namespace ArtisticSanctuarySkating
+{
+    partial class frmTemplateSubform
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblRecord = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.lblAlert = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnNew = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnPrev = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.btnFirst = new System.Windows.Forms.Button();
+            this.btnLast = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.pnlControls = new System.Windows.Forms.Panel();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pnlRecordControls = new System.Windows.Forms.Panel();
+            this.groupBox3.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.pnlControls.SuspendLayout();
+            this.pnlRecordControls.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblRecord
+            // 
+            this.lblRecord.AutoSize = true;
+            this.lblRecord.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblRecord.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.lblRecord.Location = new System.Drawing.Point(369, 8);
+            this.lblRecord.Name = "lblRecord";
+            this.lblRecord.Size = new System.Drawing.Size(54, 15);
+            this.lblRecord.TabIndex = 28;
+            this.lblRecord.Text = "lblRecord";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.btnDelete);
+            this.groupBox3.Controls.Add(this.btnUpdate);
+            this.groupBox3.Location = new System.Drawing.Point(319, 3);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(137, 79);
+            this.groupBox3.TabIndex = 26;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Current Record";
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(77, 34);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(52, 23);
+            this.btnDelete.TabIndex = 6;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(19, 34);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(52, 23);
+            this.btnUpdate.TabIndex = 5;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            // 
+            // lblAlert
+            // 
+            this.lblAlert.AutoSize = true;
+            this.lblAlert.ForeColor = System.Drawing.Color.Red;
+            this.lblAlert.Location = new System.Drawing.Point(3, 27);
+            this.lblAlert.Name = "lblAlert";
+            this.lblAlert.Size = new System.Drawing.Size(38, 13);
+            this.lblAlert.TabIndex = 27;
+            this.lblAlert.Text = "lblAlert";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnSave);
+            this.groupBox2.Controls.Add(this.btnNew);
+            this.groupBox2.Controls.Add(this.btnCancel);
+            this.groupBox2.Location = new System.Drawing.Point(462, 3);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(137, 80);
+            this.groupBox2.TabIndex = 25;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Add Record";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(15, 19);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(52, 23);
+            this.btnSave.TabIndex = 9;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // btnNew
+            // 
+            this.btnNew.Location = new System.Drawing.Point(73, 19);
+            this.btnNew.Name = "btnNew";
+            this.btnNew.Size = new System.Drawing.Size(52, 23);
+            this.btnNew.TabIndex = 7;
+            this.btnNew.Text = "New";
+            this.btnNew.UseVisualStyleBackColor = true;
+            this.btnNew.Click += new System.EventHandler(this.btnNew_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(46, 48);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(52, 23);
+            this.btnCancel.TabIndex = 8;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnPrev);
+            this.groupBox1.Controls.Add(this.btnNext);
+            this.groupBox1.Controls.Add(this.btnFirst);
+            this.groupBox1.Controls.Add(this.btnLast);
+            this.groupBox1.Location = new System.Drawing.Point(605, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(137, 80);
+            this.groupBox1.TabIndex = 24;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Select Record";
+            // 
+            // btnPrev
+            // 
+            this.btnPrev.Location = new System.Drawing.Point(36, 34);
+            this.btnPrev.Name = "btnPrev";
+            this.btnPrev.Size = new System.Drawing.Size(26, 23);
+            this.btnPrev.TabIndex = 1;
+            this.btnPrev.Text = "<";
+            this.btnPrev.UseVisualStyleBackColor = true;
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(70, 34);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(26, 23);
+            this.btnNext.TabIndex = 0;
+            this.btnNext.Text = ">";
+            this.btnNext.UseVisualStyleBackColor = true;
+            // 
+            // btnFirst
+            // 
+            this.btnFirst.Location = new System.Drawing.Point(6, 34);
+            this.btnFirst.Name = "btnFirst";
+            this.btnFirst.Size = new System.Drawing.Size(26, 23);
+            this.btnFirst.TabIndex = 5;
+            this.btnFirst.Text = "|<";
+            this.btnFirst.UseVisualStyleBackColor = true;
+            // 
+            // btnLast
+            // 
+            this.btnLast.Location = new System.Drawing.Point(102, 34);
+            this.btnLast.Name = "btnLast";
+            this.btnLast.Size = new System.Drawing.Size(26, 23);
+            this.btnLast.TabIndex = 6;
+            this.btnLast.Text = ">|";
+            this.btnLast.UseVisualStyleBackColor = true;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(670, 3);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 0;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // pnlControls
+            // 
+            this.pnlControls.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.pnlControls.Controls.Add(this.btnClose);
+            this.pnlControls.Controls.Add(this.lblRecord);
+            this.pnlControls.Controls.Add(this.lblAlert);
+            this.pnlControls.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.pnlControls.Location = new System.Drawing.Point(0, 572);
+            this.pnlControls.Name = "pnlControls";
+            this.pnlControls.Size = new System.Drawing.Size(749, 60);
+            this.pnlControls.TabIndex = 30;
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(87, 12);
+            this.txtID.Name = "txtID";
+            this.txtID.ReadOnly = true;
+            this.txtID.Size = new System.Drawing.Size(208, 20);
+            this.txtID.TabIndex = 31;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(0, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 13);
+            this.label1.TabIndex = 32;
+            this.label1.Text = "ID";
+            // 
+            // pnlRecordControls
+            // 
+            this.pnlRecordControls.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.pnlRecordControls.Controls.Add(this.groupBox3);
+            this.pnlRecordControls.Controls.Add(this.groupBox1);
+            this.pnlRecordControls.Controls.Add(this.groupBox2);
+            this.pnlRecordControls.Location = new System.Drawing.Point(0, 333);
+            this.pnlRecordControls.Name = "pnlRecordControls";
+            this.pnlRecordControls.Size = new System.Drawing.Size(749, 92);
+            this.pnlRecordControls.TabIndex = 33;
+            // 
+            // frmTemplateSubform
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(749, 632);
+            this.Controls.Add(this.pnlRecordControls);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.pnlControls);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "frmTemplateSubform";
+            this.Text = "frmTemplateSubform";
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.pnlControls.ResumeLayout(false);
+            this.pnlControls.PerformLayout();
+            this.pnlRecordControls.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Panel pnlControls;
+        public System.Windows.Forms.Button btnDelete;
+        public System.Windows.Forms.Button btnUpdate;
+        public System.Windows.Forms.Button btnSave;
+        public System.Windows.Forms.Button btnCancel;
+        public System.Windows.Forms.GroupBox groupBox1;
+        public System.Windows.Forms.Label lblRecord;
+        public System.Windows.Forms.Label lblAlert;
+        private System.Windows.Forms.Panel pnlRecordControls;
+        public System.Windows.Forms.TextBox txtID;
+        public System.Windows.Forms.Button btnPrev;
+        public System.Windows.Forms.Button btnNext;
+        public System.Windows.Forms.Button btnFirst;
+        public System.Windows.Forms.Button btnLast;
+        public System.Windows.Forms.Label label1;
+        internal System.Windows.Forms.Button btnNew;
+
+    }
+}
